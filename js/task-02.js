560c8a962f7e70cddbbfd617340716c349c01c41
dmistori84 <81422874+dmistori84@users.noreply.github.com>
2021-09-09 21:26:11 +0300

    for mentor

--- js/task-02.js
+++ js/task-02.js
@@ -8,13 +8,28 @@ const ingredients = [
   ];
 
 const listIngred = document.querySelector('ul');
-const elemListIngred = [];
 
-for (let i = 0; i < ingredients.length; i += 1) {
-  elemListIngred[i] = document.createElement('li');
-  elemListIngred[i].textContent = ingredients[i];
-  listIngred.appendChild(elemListIngred[i]);
-};
+//Вариант через обычный for
+
+//const elemListIngred;
+
+// for (let i = 0; i < ingredients.length; i += 1) {
+//   elemListIngred[i] = document.createElement('li');
+//   elemListIngred[i].textContent = ingredients[i];
+//   listIngred.appendChild(elemListIngred[i]);
+// };
+
+const createTag = array => 
+  array.map(item => {
+  const elemListIngred = document.createElement('li');
+  elemListIngred.textContent = item;
+    
+  return elemListIngred;
+  });
+ 
+
+listIngred.append(...createTag(ingredients));
+ 
 
 
 
--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,11 @@
 const input = document.getElementById('validation-input');
+const condition = input.dataset.length;
 
 input.addEventListener('blur', onChangeColor);
 
 function onChangeColor(event){
     
-    if(event.currentTarget.value.length === 6){
+    if(event.currentTarget.value.length === Number(condition)){
         input.classList = 'valid';
     } else input.classList = 'invalid';
     
